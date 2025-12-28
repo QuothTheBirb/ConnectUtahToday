098cd0aafd8b815e871bcdabb81aca1ee020c34f
QuothTheBirb <quoththebirb@proton.me>
2025-12-28 12:39:51 -0700

    Add payload Users collection

---
+++ website/collections/Users/index.ts
@@ -0,0 +1,23 @@
+import {CollectionConfig} from "payload";
+
+export const Users: CollectionConfig = {
+  slug: 'users',
+  fields: [
+    {
+      name: 'role',
+      type: 'select',
+      options: [
+        {
+          label: 'Admin',
+          value: 'admin'
+        },
+        {
+          label: 'Organizer',
+          value: 'organizer'
+        }
+      ],
+      required: true,
+      defaultValue: 'organizer'
+    }
+  ]
+}
